ec748aa4cc7a472b935fd0b4f34047c10de5193d
Marek <marek@wintermute_wsl>
2024-02-28 23:20:17 +0100

    marek: luna + querty

--- keyboards/crkbd/keymaps/marek_luna/rules.mk
+++ keyboards/crkbd/keymaps/marek_luna/rules.mk
@@ -1,12 +1,3 @@
-# Build Options
-#   change yes to no to disable
-#
-
-DEFAULT_FOLDER = crkbd/rev1
-
-RGBLIGHT_SUPPORTED = yes
-RGB_MATRIX_SUPPORTED = yes
-
 MOUSEKEY_ENABLE = YES     # Mouse keys
 RGBLIGHT_ENABLE = YES
 RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
@@ -14,6 +5,3 @@ VIA_ENABLE      = yes    # Enable VIA
 OLED_ENABLE     = yes
 LTO_ENABLE      = yes
 WPM_ENABLE = yes
-
-CONVERT_TO=rp2040_ce
-
